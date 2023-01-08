228139c96b50fc57499a4d7e8cf94e26fc6e41e9
Weerachai P <weerachai.panthueng@gmail.com>
2023-01-08 20:23:15 +0700

    Basic styling

--- src/components/SidebarHeader.js
+++ src/components/SidebarHeader.js
@@ -3,11 +3,9 @@ import "./SidebarHeader.css";
 
 const SidebarHeader = () => {
   return (
-    <div className="d-flex align-items-center sidebar mb-3 mb-md-0 text-dark text-decoration-none justify-content-between header">
-      <div className="text-start h3">Boonmafarm</div>
-      <div className="text-end btn btn-secondary">
-        <i className="fa-regular fa-bell"></i>
-      </div>
+    <div className=" sidebar text-dark text-decoration-none">
+      <div className="text-center h2">บุญมาฟาร์ม</div>
+      <hr />
     </div>
   );
 };
